3b281ffa6d9cd80b2cc498a768668ff407c33ce5
subratamondal2 <subrata24012005@gmail.com>
2025-09-06 12:29:53 +0530

    Fix: Correctly import orderReducer in rootReducer

--- frontend/src/reducer/index.js
+++ frontend/src/reducer/index.js
@@ -4,12 +4,13 @@ import authReducer from "../slices/authSlice"
 import profileReducer from "../slices/profileSlice"
 import communityReducer from "../slices/communitySlice"
 import pickerReducer from "../slices/pickerSlice"
-
+import orderReducer from "../slices/orderSlice"
 const rootReducer = combineReducers({
   auth: authReducer,
   profile: profileReducer,
   community: communityReducer,
   picker: pickerReducer,
+  order: orderReducer
 })
 
 export default rootReducer
